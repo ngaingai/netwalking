9f497b458d56aa4ded178065acc4b95eaf660667
Alex Ngai <alex.ngai@gmail.com>
2025-03-19 10:45:26 +0900

    fix: add cache-busting query parameter to logo image

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
       <div className="container flex h-14 items-center justify-between">
         <Link href="/" className="flex items-center space-x-2">
           <Image
-            src="/logo.png"
+            src="/logo.png?v=2"
             alt="NetWalking Logo"
             width={32}
             height={32}
